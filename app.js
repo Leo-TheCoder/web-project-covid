778d779ad712c05aee286ea15bf8c423309cc51a
Duc-Lam-jh <duclam227@gmail.com>
2021-12-24 22:20:39 +0700

    add profile UI

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.use('/register', registerRouter);
 app.use('/logout', logoutRouter);
 app.use('/dashboard', authenticateUser, dashboardRouter);
 app.get('/', (req, res) => res.redirect('/dashboard'));
-app.use('/profile', authenticateUser, profileRouter);
+// app.use('/profile', authenticateUser, profileRouter);
+app.use('/profile', profileRouter);
 
 // Test dashboard
 // app.use('/test', dashboardRouter);
--- controllers/users/profile.C.js
+++ controllers/users/profile.C.js
@@ -8,6 +8,7 @@ const User = require("../../models/User.M");
 
 //Moi xu li patient voi manager, admin k biet lam sao
 const getInformation = async (req, res) => {
+  /*
   const { id, type } = req.user;
 
   const result = await User.getInformation(id, type);
@@ -15,9 +16,19 @@ const getInformation = async (req, res) => {
   if (!result) {
     throw new NotFoundError("Something wrong, please try again!");
   }
-
   //need adjust, not send all attributes back
-  res.json({ result });
+ */
+  res.render('user/profile',{
+    user: "okay",
+    userFullname: "Lâm Xương Đức",
+    userDoB: "22/07/2000",
+    userPhoneNumber: "0123458755",
+    userAddress: "dasdasd/adasd ",
+  }
+
+  )
+
+  // res.json({ result });
 };
 
 const updateInformation = async (req, res) => {
