02393696c704cb15bb473a7afd3329dc351adeb7
Duc-Lam-jh <duclam227@gmail.com>
2021-12-27 21:29:09 +0700

    add edit profile

--- controllers/users/profile.C.js
+++ controllers/users/profile.C.js
@@ -16,7 +16,7 @@ const getInformation = async (req, res) => {
   }
 
   if (type === "P") {
-    res.status(StatusCodes.OK).render("user/profile", {
+    res.status(StatusCodes.OK).render("user/profile/profile", {
       user: "okay",
       userFullname: result.patientname,
       userDoB: Utility.getDDMMYYYYFormat(result.patientdob),
@@ -24,7 +24,7 @@ const getInformation = async (req, res) => {
       userAddress: result.patientaddress,
     });
   } else if (type === "M") {
-    res.status(StatusCodes.OK).render("user/profile", {
+    res.status(StatusCodes.OK).render("user/profile/profile", {
       user: "okay",
       userFullname: result.managername,
       userDoB: Utility.getDDMMYYYYFormat(result.managerdob),
@@ -45,8 +45,50 @@ const getInformation = async (req, res) => {
   // res.json({ result });
 };
 
+const editInformation = async (req, res) => {
+  const { id, type } = req.user;
+
+  const result = await User.getInformation(id, type);
+
+  if (!result) {
+    throw new NotFoundError("Something wrong, please try again!");
+  }
+
+  const dob = result.patientdob;
+  let month = dob.getMonth();
+  if(++month <= 10) {
+    month = '0' + month;
+  }
+  if (type === "P") {
+    res.status(StatusCodes.OK).render("user/profile/editProfile", {
+      user: "okay",
+      userFullname: result.patientname,
+      userDoB: dob.getFullYear() + '-' + month + '-' + dob.getDate(),
+      userAddress: result.patientaddress,
+    });
+  } else if (type === "M") {
+    res.status(StatusCodes.OK).render("user/profile/editProfile", {
+      user: "okay",
+      userFullname: result.managername,
+      userDoB: dob.getFullYear() + '-' + month + '-' + dob.getDate(),
+      userAddress: result.manageraddress,
+    });
+  }
+  else {  //dummy
+    res.render("user/profile/edit", {
+      user: "okay",
+      userFullname: "Who the hell are you",
+      userDoB: "xx/xx/xxxx",
+      userAddress: "666",
+    });
+  }
+
+  // res.json({ result });
+};
+
 const updateInformation = async (req, res) => {
   const { id, type } = req.user;
+  console.log(req.body);
 
   const result = await User.updateInformation(id, type, req.body);
 
@@ -62,4 +104,5 @@ const updateInformation = async (req, res) => {
 module.exports = {
   getInformation,
   updateInformation,
+  editInformation,
 };
