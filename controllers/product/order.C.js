9e0f979ec7c4e319dd1f62c4d014e1df840ce3df
Kim Nguyễn Duy Thiên <ngduytkim@gmail.com>
2022-01-18 02:50:13 +0700

    order history feature

--- controllers/product/order.C.js
+++ controllers/product/order.C.js
@@ -1,39 +1,40 @@
-const { StatusCodes } = require('http-status-codes');
-const { CustomError } = require('../../errors');
-const Order = require('../../models/Order.M');
+/** @format */
 
-const getPatientOrders = async(req, res) => {
-    let result;
-    if(req.user.type === 'P')
-    {
-        result = await Order.getPersonalPatientOrders(req.user.mainId);
-    }
-    else if(req.user.type === 'M')
-    {
-        result = await Order.getPatientOrders(req.user.mainId);
-    }
+const { StatusCodes } = require("http-status-codes");
+const { CustomError } = require("../../errors");
+const Order = require("../../models/Order.M");
 
-    if(!result)
-    {
-        throw new CustomError("Something wrong with get list Order");
-    }
+const getPatientOrders = async (req, res) => {
+	let result;
+	if (req.user.type === "P") {
+		result = await Order.getPersonalPatientOrders(req.user.mainId);
+	} else if (req.user.type === "M") {
+		result = await Order.getPatientOrders(req.user.mainId);
+	}
 
-    res.status(StatusCodes.OK).json({result});
-}
+	if (!result) {
+		throw new CustomError("Something wrong with get list Order");
+	}
 
-const getOrderDetail = async(req, res) => {
-    const {orderId} = req.params;
+	console.log(result);
+	res.render("products/orderhistory", {
+		orders: result,
+	});
+};
 
-    const result = await Order.getOrderDetail(orderId);
+const getOrderDetail = async (req, res) => {
+	const { orderId } = req.params;
 
-    if(!result) {
-        throw new CustomError("Something wrong with get order detail");
-    }
+	const result = await Order.getOrderDetail(orderId);
 
-    res.status(StatusCodes.OK).json({result});
-}
+	if (!result) {
+		throw new CustomError("Something wrong with get order detail");
+	}
+
+	res.status(StatusCodes.OK).json({ result });
+};
 
 module.exports = {
-    getPatientOrders,
-    getOrderDetail,
-}
+	getPatientOrders,
+	getOrderDetail,
+};
