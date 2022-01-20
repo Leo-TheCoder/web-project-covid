37d9f3ff2293e6f5db8cce162a573cf5b8beed39
Leo-TheCoder <64176716+Leo-TheCoder@users.noreply.github.com>
2022-01-21 04:07:19 +0700

    Test

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
+app.get('/', (req, res) => res.send('Hello'));
 
 const loginRouter = require("./routes/auth_routes/login_route");
 const logoutRouter = require("./routes/auth_routes/logout_route");
