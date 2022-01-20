9be18d147f5dccbbdf265f9d138ed924f8928ff7
Kim Nguyễn Duy Thiên <ngduytkim@gmail.com>
2022-01-20 12:59:40 +0700

    update patient changes

--- public/js/editpatient.js
+++ public/js/editpatient.js
@@ -1,4 +1,5 @@
 /** @format */
+
 async function deletePatient(patientId) {
 	console.log("delete patient");
 
@@ -13,12 +14,21 @@ async function deletePatient(patientId) {
 	console.log(response);
 }
 
-async function updatePatient(patientId) {
+async function updatePatient(patientId, curStatus, curArea) {
 	console.log("update patient step 2");
 	const status = document.getElementById("status").value;
 	const area = document.getElementById("hospital-area").value;
+	console.log(curStatus);
+	console.log(status);
+	console.log(curArea);
+	console.log(area);
 
-	const body = {status, area};
+	if (curStatus == status && curArea == area) {
+		console.log("not updated");
+		return;
+	}
+
+	const body = { status, area };
 	const response = await fetch(url + `/patients/${patientId}`, {
 		method: "PUT",
 		headers: {
