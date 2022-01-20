b549f72fdf68ca8d9251b6d0df1aa21a22120695
Leo-TheCoder <64176716+Leo-TheCoder@users.noreply.github.com>
2022-01-20 16:05:16 +0700

    Add pay route

--- controllers/product/cart.C.js
+++ controllers/product/cart.C.js
@@ -1,36 +1,39 @@
 const { StatusCodes } = require("http-status-codes");
 const { CustomError, NotFoundError } = require("../../errors");
 const Cart = require("../../models/Cart.M");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User.M");
 
 const addToCart = async (req, res) => {
   const result = await Cart.addToCart(req.body, req.patientid);
-  
+
   if (!result) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       msg: "Insert failed!",
       status: "Fail",
     });
   }
-  
-  return res.status(StatusCodes.OK).redirect('/packs');
+
+  return res.status(StatusCodes.OK).redirect("/packs");
 };
 
 const getItemsInCart = async (req, res) => {
   const result = await Cart.getItems(req.patientid);
-  result.forEach(pack => {
+  result.forEach((pack) => {
     let totalCash = 0.0;
-    pack.products.forEach(product => {
-      product.cash = parseFloat(product.productprice) * parseInt(product.quantity);
+    pack.products.forEach((product) => {
+      product.cash =
+        parseFloat(product.productprice) * parseInt(product.quantity);
       totalCash += product.cash;
-    })
+    });
     pack.total = totalCash;
-  })
+  });
 
   res.status(StatusCodes.OK).render("patients/cart/cart", {
-		cart: result,
+    cart: result,
     editScript: () => "editcartscript",
-		user: true,
-	});
+    user: true,
+  });
 };
 
 const getItemById = async (req, res) => {
@@ -51,20 +54,26 @@ const updateItemQuantityInCart = async (req, res) => {
   const afterDelete = await Cart.getItems(req.patientid);
 
   if (!result || result < 1) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      msg: "Update failed!",
-      status: "Fail",
-    }).render("patients/cart/cart", {
-      cart: afterDelete,
-      editScript: () => "editcartscript",
-      user: true,
-    });;
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        msg: "Update failed!",
+        status: "Fail",
+      })
+      .render("patients/cart/cart", {
+        cart: afterDelete,
+        editScript: () => "editcartscript",
+        user: true,
+      });
   }
 
-  res.status(StatusCodes.OK).json({
-    msg: "Update successfully!",
-    status: "Success",
-  }).render();
+  res
+    .status(StatusCodes.OK)
+    .json({
+      msg: "Update successfully!",
+      status: "Success",
+    })
+    .render();
 };
 
 const deletePackInCart = async (req, res) => {
@@ -84,10 +93,41 @@ const deletePackInCart = async (req, res) => {
   });
 };
 
+const pay = async (req, res) => {
+  const {cartId} = req.params;
+  const { total, details } = req.body;
+
+  const user = await User.getInformation(req.user.mainId, "P");
+  const url = req.protocol + '://' + req.get('host') + req.originalUrl + '/result';
+  
+  const orderToken = jwt.sign(
+    {
+      iat: Date.now(),
+      iss: "vulong61@gmail.com",
+      cus: user.patientphone,
+      amt: total,
+      msg: "Payment of cart id: " + cartId,
+      cbu: url,
+    },
+    process.env.API_PAYMENT_KEY,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+
+  return res.send(orderToken);
+};
+
+const payResult = async (req, res) => {
+  return res.send();
+}
+
 module.exports = {
   addToCart,
   getItemsInCart,
   getItemById,
   updateItemQuantityInCart,
   deletePackInCart,
+  pay,
+  payResult,
 };
--- routes/cart_route.js
+++ routes/cart_route.js
@@ -7,10 +7,13 @@ const {
   getItemById,
   updateItemQuantityInCart,
   deletePackInCart,
+  pay,
+  payResult,
 } = require("../controllers/product/cart.C");
 
 router.route("/").get(getItemsInCart).post(addToCart);
 //router.route("/add").get(addProduct);
-router.route("/:cartId").delete(deletePackInCart);
+router.route("/:cartId").delete(deletePackInCart).post(pay);
+router.route("/:cartId/result").post(payResult);
 
 module.exports = router;
